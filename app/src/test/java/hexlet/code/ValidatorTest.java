9fce7f4a97a404e2e6cb06211964f742a3f31a2d
Hisun0 <tridvad1@gmail.com>
2024-05-05 20:35:56 +0300

    first step

---
+++ app/src/test/java/hexlet/code/ValidatorTest.java
@@ -0,0 +1,34 @@
+package hexlet.code;
+
+import hexlet.code.Validator;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ValidatorTest {
+    @Test
+    public void stringTest() {
+        Validator validator = new Validator();
+        StringSchema schema = validator.string();
+
+        assertTrue(schema.isValid(""));
+        assertTrue(schema.isValid(null));
+        assertFalse(schema.isValid(5));
+
+        schema.required();
+
+        assertFalse(schema.isValid(""));
+        assertFalse(schema.isValid(null));
+        assertTrue(schema.isValid("hello from the other side"));
+        assertTrue(schema.isValid("seven nation army"));
+
+        assertTrue(schema.contains("sev").isValid("seven nation army"));
+        assertTrue(schema.contains("seven").isValid("seven nation army"));
+        assertFalse(schema.contains("sevennation").isValid("seven nation army"));
+
+        assertFalse(schema.isValid("seven nation army"));
+
+        StringSchema schema1 = validator.string();
+        assertTrue(schema1.minLength(10).minLength(4).isValid("hello"));
+    }
+}
